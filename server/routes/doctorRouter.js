caf3b40323a58d06531f219c6d23fde8644d4a59
soumyarajbag <soumyarajbag@gmail.com>
2023-11-18 18:15:34 +0530

    Server seperated

--- server/routes/doctorRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addDoctor, getDoctors } = require("../controllers/doctorController");
-
-const doctorRouter = express.Router();
-
-doctorRouter.post("/add", addDoctor);
-doctorRouter.get("/", getDoctors);
-
-module.exports = doctorRouter;
